5c6bccc16371ac5fda24de2577565e22487ff02c
ilhom2497 <ilhom2497@gmail.com>
2024-06-15 13:17:46 -0500

    added logic to my temp converter

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ document.getElementById("tempConverter").addEventListener("submit", function (ev
     if (unit === "C" && convertIn === "F") {
         convertedValue = (degree * 9/5) + 32; // To convert temperatures in degrees Celsius to Fahrenheit, multiply by 1.8 (or 9/5) and add 32.
     } else if (unit === "F" && convertIn === "C") {
-        convertedValue = (degree - 32) * 5/9;
+        convertedValue = (degree - 32) * 5/9; // To convert temperatures in degrees Fahrenheit to Celsius, subtract 32 and multiply by .5556 (or 5/9).
     } else {
         convertedValue = degree;
     }
