a97e6714c11d8ecbf99009c84663daa905a6bbc3
ilhom2497 <ilhom2497@gmail.com>
2024-06-15 13:13:31 -0500

    added logic to my temp converter

---
+++ main.js
@@ -0,0 +1,19 @@
+document.getElementById("tempConverter").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const degree = parseFloat(document.getElementById("degree").value);
+    const unit = document.getElementById("unit").value;
+    const convertIn = document.getElementById("convertIn").value;
+
+    let convertedValue;
+    if (unit === "C" && convertIn === "F") {
+        convertedValue = (degree * 9/5) + 32; // To convert temperatures in degrees Celsius to Fahrenheit, multiply by 1.8 (or 9/5) and add 32.
+    } else if (unit === "F" && convertIn === "C") {
+        convertedValue = (degree - 32) * 5/9;
+    } else {
+        convertedValue = degree;
+    }
+
+    document.getElementById("resultValue").textContent = convertedValue.toFixed(2);
+    document.getElementById("resultUnit").textContent = `${convertIn}`;
+})
